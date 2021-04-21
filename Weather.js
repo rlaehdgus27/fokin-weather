dfde3a29ba633827b928915af2899bb16026370a
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-04-21 20:01:21 +0900

    #2.5

--- Weather.js
+++ Weather.js
@@ -8,22 +8,32 @@ const weatherOptions = {
   Thunderstorm: {
     iconName: "weather-lightning",
     gradient: ["#373B44", "#4286f4"],
+    title: "천둥번개",
+    subtitle: "제발 집에만 있어!",
   },
   Drizzle: {
     iconName: "weather-hail",
     gradient: ["#89F7FE", "#66A6FF"],
+    title: "이슬비",
+    subtitle: "이런 날씨에는 파전에 막걸리!",
   },
   Rain: {
     iconName: "weather-rainy",
     gradient: ["#00C6FB", "#005BEA"],
+    title: "비",
+    subtitle: "이런 날씨에는 파전에 막걸리!",
   },
   Snow: {
     iconName: "weather-snowy",
     gradient: ["#7DE2FC", "#B9B6E5"],
+    title: "눈",
+    subtitle: "눈쓸꺼 아니면 나가지마!",
   },
   Atmosphere: {
     iconName: "weather-hail",
     gradient: ["#89F7FE", "#66A6FF"],
+    title: "대기",
+    subtitle: "대기가 먼데..",
   },
   Clear: {
     iconName: "weather-sunny",
@@ -34,18 +44,26 @@ const weatherOptions = {
   Clouds: {
     iconName: "weather-cloudy",
     gradient: ["#D7D2CC", "#304352"],
+    title: "우중충",
+    subtitle: "흐리니까 형 건들지 마라!.. ",
   },
   Mist: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
+    title: "안개",
+    subtitle: "앞이 보이지 않아!!...",
   },
   Dust: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
+    title: "황사",
+    subtitle: "개 같은 짱깨새끼들",
   },
   Haze: {
     iconName: "weather-hail",
     gradient: ["#4DA0B0", "#D39D38"],
+    title: "안개",
+    subtitle: "앞이 보이지 않아!!...",
   },
 };
 
@@ -66,7 +84,7 @@ export default function Weather({ temp, condition }) {
         <Text style={styles.temp}>{temp}°</Text>
       </View>
 
-      <View style={{ ...styles.halfContainer, ...styles.textContainer }}>
+      <View style={styles.textContainer}>
         <Text style={styles.title}>{weatherOptions[condition].title}</Text>
         <Text style={styles.subtitle}>
           {weatherOptions[condition].subtitle}
@@ -95,8 +113,6 @@ Weather.prototype = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
   temp: {
     fontSize: 42,
@@ -112,14 +128,18 @@ const styles = StyleSheet.create({
     fontSize: 44,
     fontWeight: "300",
     marginBottom: 10,
+    textAlign: "left",
   },
   subtitle: {
     color: "white",
     fontSize: 24,
     fontWeight: "600",
+    textAlign: "left",
   },
   textContainer: {
-    paddingHorizontal: 20,
     alignItems: "flex-start",
+    paddingHorizontal: 40,
+    justifyContent: "center",
+    flex: 1,
   },
 });
