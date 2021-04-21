1ce90c0ed04b62207ba28ee6e84a43fdc1cc6d8f
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-04-21 18:13:02 +0900

    #1.4

--- App.js
+++ App.js
@@ -2,11 +2,21 @@ import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+
+const API_KEY = "2b59ef9a594a554bd714993877a89548";
 
 export default class extends React.Component {
   state = {
     isLoading: true,
   };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log(data);
+  };
+
   getLocation = async () => {
     try {
       await Location.getForegroundPermissionsAsync();
@@ -14,6 +24,7 @@ export default class extends React.Component {
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("어디있는지 몰라서 날씨 정보를 가져 올 수 없어요");
