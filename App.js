9fa268474035622f2f10a4f5ba7b12c8d16fd618
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-04-21 17:41:40 +0900

    #1.3

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
 import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
   getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      await Location.getForegroundPermissionsAsync();
+
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("어디있는지 몰라서 날씨 정보를 가져 올 수 없어요");
+    }
   };
   componentDidMount() {
     this.getLocation();
   }
   render() {
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
